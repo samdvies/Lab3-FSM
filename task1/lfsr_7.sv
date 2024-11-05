9e4aa219504adad508056c77c51561d83d43cdd4
samdvies <daviessam003@gmail.com>
2024-11-05 15:05:03 +0000

    task 1 challenge working

--- task1/lfsr_7.sv
+++ task1/lfsr_7.sv
@@ -1,8 +1,16 @@
-module lfsr_7 (
+module lfsr(
     input   logic       clk,
     input   logic       rst,
     input   logic       en,
     output  logic [6:0] data_out
 );
-
+logic [7:1] sreg;
+always_ff @ (posedge clk, posedge rst)
+    if(rst) begin
+        sreg <= {6'b0,1'b1};
+    end
+    else begin
+        sreg <={sreg[6:1] , sreg[7] ^ sreg[3]};
+    end
+assign data_out = sreg;
 endmodule
