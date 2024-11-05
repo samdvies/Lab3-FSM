097c0264d9fb87dc7ff7bb64c82667604f90b147
samdvies <daviessam003@gmail.com>
2024-11-05 14:29:41 +0000

    4 bit lfsr working

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -4,5 +4,13 @@ module lfsr(
     input   logic       en,
     output  logic [3:0] data_out
 );
-
+logic [4:1] sreg;
+always_ff @ (posedge clk, posedge rst)
+    if(rst) begin
+        sreg <= 1;
+    end
+    else begin
+        sreg <= {sreg[3:1], sreg[4] ^ sreg[3]};
+    end
+assign data_out = sreg;
 endmodule
