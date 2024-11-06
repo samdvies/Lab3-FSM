b96f9c6f75366949f463bdd0ef01165a1eef708c
samdvies <daviessam003@gmail.com>
2024-11-06 09:59:07 +0000

    f1 lights w/o delay working

--- task2/f1_fsm.sv
+++ task2/f1_fsm.sv
@@ -5,4 +5,19 @@ module f1_fsm (
     output  logic [7:0] data_out
 );
 
+logic [8:1] fsm;
+always_ff @ (posedge clk, posedge rst)
+    if (rst) begin
+        fsm <=0;
+    end
+    else if(en) begin
+        if (fsm == 255) begin
+            fsm <=0;
+        end
+        else begin
+            fsm <= {fsm[7:1], 1'b1};
+        end
+    end
+
+assign data_out = fsm;
 endmodule
